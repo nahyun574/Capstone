e56dc3b16507d6b656305ab9fb1f1ee0acdf7d5e
OH Yunju <84166982+OH-YUNJU@users.noreply.github.com>
2023-06-17 22:07:13 +0900

    Update guideline.py

--- Demo/guideline.py
+++ Demo/guideline.py
@@ -6,7 +6,10 @@ from Library import *
 #* 어깨 : x 148,496 | y 256,480
 #* 귀 : x 256,374 | y 109,183
 
-#*아직은 출력만
+
+##############어깨##############
+
+#어깨 깊이
 def Shoulder_Depth():
     #두 어깨의 깊이 차이가 0.1 이상일 때 => 카메라와 평행으로 서주세요
     if(abs(L_SHOULDER.z - R_SHOULDER.z) > 0.1):
@@ -19,13 +22,12 @@ def Shoulder_Depth():
     
     #코의 좌표와 어깨 중앙의 좌표를 기준으로 
     Middle_dist = (L_SHOULDER.z + R_SHOULDER.z) / 2
-    if(abs(NOSE.z - Middle_dist) > 0.2):
+    if(abs(NOSE.z - Middle_dist) > 0.1):
         STR.guide = '허리를 펴주세요'
-    #*
     else:
         print('코의 깊이 : ({}, {}) : {} 미터'.format(NOSE.x, NOSE.y, NOSE.z))
 
-
+#어깨 가이드라인
 def INguideline(Depth,Landmarks):
     try :
         #어깨 좌표가 이미지 안에 들어와있는지 확인 및 좌표 가져오기
@@ -34,15 +36,15 @@ def INguideline(Depth,Landmarks):
         L_EAR.x, L_EAR.y = Landmarks.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_EAR].x * WIDTH, Landmarks.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_EAR].y * HEIGHT
         R_EAR.x, R_EAR.y = Landmarks.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_EAR].x * WIDTH, Landmarks.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_EAR].y * HEIGHT
         NOSE.x, NOSE.y = Landmarks.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].x * WIDTH, Landmarks.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].y * HEIGHT
-
+        
         L_SHOULDER.z = Depth.get_distance(int(L_SHOULDER.x), int(L_SHOULDER.y))
         R_SHOULDER.z = Depth.get_distance(int(R_SHOULDER.x), int(R_SHOULDER.y))
         NOSE.z = Depth.get_distance(int(NOSE.x), int(NOSE.y))
 
         
         #가이드라인 안에 있는지 확인
-        if(148 < L_SHOULDER.x < 496 and 265 < L_SHOULDER.y < 480 and 148 < R_SHOULDER.x < 496 and 265 < R_SHOULDER.y < 480):
-            if(258 < L_EAR.x < 374 and 109 < L_EAR.y < 183 and 258 < R_EAR.x < 374 and 109 < R_EAR.y < 183): #가이드라인 안에서
+        if(320 < L_SHOULDER.x < 501 and 225 < L_SHOULDER.y < 480 and 137 < R_SHOULDER.x < 320 and 225 < R_SHOULDER.y < 480):
+            if(254 < L_EAR.x < 387 and 82 < L_EAR.y < 225 and 254 < R_EAR.x < 387 and 82 < R_EAR.y < 225): #가이드라인 안에서
                 #측정 시작부분
                 return True
         
@@ -59,6 +61,10 @@ def INguideline(Depth,Landmarks):
     
     return False
 
+
+##############얼굴###############
+
+#얼굴 어깨 수직선
 def INFace_shoulder(Depth,Landmarks):
     try :
         #어깨 좌표가 이미지 안에 들어와있는지 확인 및 좌표 가져오기
@@ -69,9 +75,8 @@ def INFace_shoulder(Depth,Landmarks):
         STR.guide = '카메라안으로 들어와주세요'
     
     return False
-        
- 
 
+#얼굴 가이드라인
 def INFace(Depth,Landmarks):
     try :
         face_landmarks = Landmarks.multi_face_landmarks[0] 
